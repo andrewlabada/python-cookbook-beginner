ec4c4ceed7538ef1b42feb637ce3b713b208b8bf
sadreactsonle <labadaandrew@gmail.com>
2022-10-22 20:17:37 +0800

    ch1 constructing dictionaries without excessive typing

---
+++ src/chapter1/constructing_dictionary_without_excessive_quoting/example.py
@@ -0,0 +1,18 @@
+#data = { 'red' : 1, 'green' : 2, 'blue' : 3 }
+
+def makedict(**kwargs):
+    return kwargs
+
+data  = makedict(red=1, green=2, blue=3)
+
+print(data)
+
+def dodict(*args, **kwds):
+    d = {}
+    for k, v in args: d[k] = v
+    d.update(kwds)
+    return d
+
+tada = dodict(*data.items( ), yellow=2, green=4)
+
+print(tada)
